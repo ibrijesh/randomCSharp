ed568d2b80abbf203f855d6cf530e63eeefd5a52
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-01-20 14:09:37 +0530

    Array

--- randomCSharp/Random.cs
+++ randomCSharp/Random.cs
@@ -2,17 +2,19 @@ namespace randomCSharp;
 
 public class Random
 {
-    public static void DisplayInfo(string name, int age, string city)
-    {
-        Console.WriteLine($"Name: {name}, Age: {age}, City: {city}");
-    }
-
     public static void Learn()
     {
-        // Using named parameters to pass arguments in any order
-        DisplayInfo(age: 30, name: "Alice", city: "New York");
+        // Array
+        int[] array = new int[4];
 
-        // You can still call it using positional arguments if needed
-        DisplayInfo("Bob", 25, "Los Angeles");
+        array[0] = 47;
+        array[1] = 77;
+        array[2] = 87;
+        array[3] = 97;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
     }
 }
