32b60095a2719e4daef5c19d96eec139b1b37ed7
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2025-01-20 14:11:15 +0530

    ArrayList

--- randomCSharp/Random.cs
+++ randomCSharp/Random.cs
@@ -1,20 +1,23 @@
+using System.Collections;
+
 namespace randomCSharp;
 
 public class Random
 {
     public static void Learn()
     {
-        // Array
-        int[] array = new int[4];
+        // Array List
+        ArrayList arrayList = new ArrayList();
 
-        array[0] = 47;
-        array[1] = 77;
-        array[2] = 87;
-        array[3] = 97;
+        arrayList.Add("Ajay");
+        arrayList.Add("Ankit");
+        arrayList.Add(10);
+        arrayList.Add(10.10);
 
-        for (int i = 0; i < array.Length; i++)
+
+        for (int i = 0; i < arrayList.Count; i++)
         {
-            Console.WriteLine(array[i]);
+            Console.WriteLine(arrayList[i]);
         }
     }
 }
